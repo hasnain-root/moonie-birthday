037fdc79dcab65c97107dea1c11105149732c712
Hasnain <hasnain@ibees.biz>
2024-12-05 02:49:36 +0500

    few changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 const title = document.querySelector('.title')
-const text = 'To The Most Beautiful Soul'.split('')
+const text = 'To The Most Beautiful Soul>>  "Moonie" '.split('')
+// for (let index = 0; index < text.length; index++) {
+//   if (text[index] !== ' ') {
+//     title.innerHTML += `<span>${text[index]}<span/>`
+//   } else {
+//     title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+//   }
+// }
 for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+  if (text[index] !== ' ' && text[index] !== '\n') {
+    title.innerHTML += `<span>${text[index]}<span/>`;
+  } else if (text[index] === '\n') {
+    title.innerHTML += `<br/>`;  // Add a line break when you encounter \n
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`;
   }
 }
 
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Mooniee 🎂').split('')
+    const titles = ('🎂 Happy Birthday Moonieee *^_^* 🎂').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -11,7 +11,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 100); // 1000ms delay
       }
     }
 
